06d6d13ed29f5bc2f46c3ee317d00b3a7cf142f4
Jessaleem <aleejess@gmail.com>
2023-01-23 15:28:10 -0500

    feat: upload data to Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,24 @@
+import { useEffect, useState } from 'react';
+import { getAllData } from '../services/data';
+
 function Home() {
-  return <h1> Hola Mundo!</h1>;
+  const [showData, setShowData] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getAllData();
+        setShowData(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  console.log(showData);
+
+  return <h1>Hola Mundo!</h1>;
 }
 
 export default Home;
