1099ab958d774c89f705205c32e18eab28004426
Jessaleem <aleejess@gmail.com>
2023-02-15 23:02:52 -0500

    feat: add map to show info in Detail

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactPlayer from 'react-player';
 
 function Detail({ detail }) {
-  console.log(detail);
+  // console.log(detail);
+  let ids = new Array(detail.genre?.length).fill().map((_, i) => i);
+  console.log(ids);
   return (
     <div className='flex justify-center flex-col'>
       <div className='rounded flex justify-center'>
-        <ReactPlayer url={detail.trailer} playing controls loops />
+        {/* <ReactPlayer url={detail.trailer} playing controls loops /> */}
       </div>
       <div className='flex space-x-4 justify-center py-5'>
         <div>
@@ -14,9 +16,19 @@ function Detail({ detail }) {
           <p className='text-white max-w-md'>{detail.description}</p>
         </div>
         <div>
-          <p className='text-white'>Director:</p>
-          <p className='text-white'>Genre:</p>
-          <p className='text-white'>Writters:</p>
+          <p className='text-white'>Director: {detail.director}</p>
+          <div className='text-white'>
+            Genre:
+            {detail.genre?.map((item, index) => {
+              return <span key={index}> {item} </span>;
+            })}
+          </div>
+          <div className='text-white'>
+            Writters:
+            {detail.writers.map((writer, index) => {
+              return <span key={index}> {writer}</span>;
+            })}
+          </div>
         </div>
       </div>
     </div>
